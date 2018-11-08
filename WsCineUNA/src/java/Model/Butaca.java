38ee86e41387a55b618f92baf7f77734f50256b7
Rodrigo Vargas <42876568+ravargas111@users.noreply.github.com>
2018-11-08 11:04:27 -0600

    cargando activas

--- WsCineUNA/src/java/Model/Butaca.java
+++ WsCineUNA/src/java/Model/Butaca.java
@@ -23,8 +23,6 @@ import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
