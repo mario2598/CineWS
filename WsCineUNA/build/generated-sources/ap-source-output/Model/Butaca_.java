9872fbeaf8a4d1eff8ce7cbdd549cc7e493569a1
Chris Fallas <35886616+Chris-Fallas@users.noreply.github.com>
2018-11-10 15:24:10 -0600

    lib ref

---
+++ WsCineUNA/build/generated-sources/ap-source-output/Model/Butaca_.java
@@ -0,0 +1,26 @@
+package Model;
+
+import Model.Comprobante;
+import Model.Reserva;
+import Model.Sala;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-11-10T15:23:34")
+@StaticMetamodel(Butaca.class)
+public class Butaca_ { 
+
+    public static volatile SingularAttribute<Butaca, String> butActiva;
+    public static volatile SingularAttribute<Butaca, String> butImg;
+    public static volatile SingularAttribute<Butaca, Long> butFila;
+    public static volatile SingularAttribute<Butaca, String> butLetra;
+    public static volatile ListAttribute<Butaca, Reserva> reservaList;
+    public static volatile SingularAttribute<Butaca, Long> butId;
+    public static volatile SingularAttribute<Butaca, Sala> salaId;
+    public static volatile SingularAttribute<Butaca, Long> butColumna;
+    public static volatile SingularAttribute<Butaca, Reserva> resId;
+    public static volatile ListAttribute<Butaca, Comprobante> comprobanteList;
+
+}
